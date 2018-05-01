111fffa9855f770cb42abe4f75e8e53ca7794363
jamesliao2016 <453143942@qq.com>
2018-05-01 17:13:55 +0800

    update

--- data_process.py
+++ data_process.py
@@ -1,2 +1,14 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import statsmodels.api as sm
+from statsmodels.nonparametric.kde import KDEUnivariate
+from statsmodels.nonparametric import smoothers_lowess
+from pandas import Series, DataFrame
+from patsy import dmatrices
+from sklearn import datasets, svm
+
+inputfile = "rawData/train.csv"
+traindata = pd.read_csv(inputfile)
+
+
