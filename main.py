61e3c7126d074d6cb7d8ac9274b59b0ea431c966
jamesliao2016 <453143942@qq.com>
2018-05-02 23:16:32 +0800

    3

--- main.py
+++ main.py
@@ -10,25 +10,39 @@ from patsy import dmatrices
 from sklearn import datasets, svm
 # import xgboost as xgb
 
-if __name__ == '__main__':
-    inputfile = "rawData/train.csv"
-    traindata = pd.read_csv(inputfile)
+from sklearn import tree
 
+if __name__ == '__main__':
     inputfile2 = "rawData/test.csv"
     testdata = pd.read_csv(inputfile2)
+    inputfile = "rawData/train.csv"
+    traindata = pd.read_csv(inputfile)
+    traindata = traindata.replace(np.NAN, 0)
+    traindata['Sex'] = traindata['Sex'].apply(lambda x:
+                                              1 if x=='male' else 0)
+    traindata['Embarked'] = traindata['Embarked'].apply(lambda x:0 if x=='S' else (1 if x=='C' else 2))
 
-    from sklearn import tree
+    train_x = traindata.loc[:, ['Embarked','Fare','Age','Pclass','Sex','SibSp','Parch']]
+    train_y = traindata.loc[:,['Survived']]
+
+    clf = tree.DecisionTreeClassifier()
+    # clf = LogisticRegression()
 
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(train_x, train_y)
 
-    test_x = testdata.loc[:, ['Fare']]
+    test_x = testdata.loc[:, ['Embarked','Fare','Age','Pclass','Sex','SibSp','Parch']]
+    test_x['Sex'] = test_x['Sex'].apply(lambda x:
+                                              1 if x=='male' else 0)
+    test_x['Embarked'] = test_x['Embarked'].apply(lambda x:0 if x=='S' else (1 if x=='C' else 2))
+
     test_x = test_x.replace(np.NAN, 0)
 
     test_y = clf.predict(test_x)
+    outputdf = pd.DataFrame(test_y, columns=['Survived'])
 
     outputdf2 = testdata.loc[:, ['PassengerId']].merge(outputdf, left_index=True, right_index=True)
-    outputdf2.to_csv("submission.csv", index=False)
+    outputdf2.to_csv("submission1.csv", index=False)
 
 
 
--- train_model.py
+++ train_model.py
@@ -5,28 +5,39 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from test import testfunc
 # import xgboost as xgb
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
 if __name__ == '__main__':
     inputfile = "rawData/train.csv"
     traindata = pd.read_csv(inputfile)
     traindata = traindata.replace(np.NAN, 0)
+    traindata['Sex'] = traindata['Sex'].apply(lambda x:
+                                              1 if x=='male' else 0)
+    traindata['Embarked'] = traindata['Embarked'].apply(lambda x:0 if x=='S' else (1 if x=='C' else 2))
 
     # inputfile2 = "rawData/test.csv"
     # testdata = pd.read_csv(inputfile2)
 
     from sklearn import tree
     train_x, test_x, train_y, test_y = train_test_split(
-        traindata.loc[:, ['Fare']], traindata.loc[:,['Survived']], test_size=0.33)
+        traindata.loc[:, ['Embarked','Fare','Age','Pclass','Sex','SibSp','Parch']], traindata.loc[:,['Survived']], test_size=0.33)
+    print (train_x.index.size)
+    print(test_x.index.size)
+    clf = tree.DecisionTreeClassifier()
+    # clf = LogisticRegression()
+
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(train_x, train_y)
 
     predict_y = clf.predict(test_x)
     predict_y = pd.DataFrame(predict_y, columns=['Survived'])
+    predict_y.head(5)
 
     print (testfunc(predict_y, test_y, 'Survived'))
 
     # outputdf2 = testdata.loc[:, ['PassengerId']].merge(outputdf, left_index=True, right_index=True)
-    # outputdf2.to_csv("submission.csv", index=False)
+    # outputdf2.to_csv("submission1.csv", index=False)
 
 
 
